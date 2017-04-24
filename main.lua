32aa7aedf1ef88c0556789004ec5ea4cbdd3f6f7
Julian Kuo-Gross <julian.kuo-gross19@cs81-us-ct3-19.local>
2017-04-24 14:43:07 -0400

    ianStuffs

--- main.lua
+++ main.lua
@@ -2,46 +2,38 @@ function love.load(  )
 	x = 1
 	y = 0
 	dx = 0
-	dy = 1
-	tableOfFloors = {{0, 64, 120}}
-	airborne = true
+	dy = 0
+	bx = 0
+	by = 0
+	bdx = 0
+	bdy = 0
 end
 
 function love.update( dt )
 	x = dx + x
-	y = y + dy
-	if (airborne) then		
-		y = dy + 1
-
-		for i,v in ipairs(tableOfFloors) do
-			if	y > v[3] and x > v[1] - 64 and x < v[2] + 64 and y - dy <= v[3]  then
-				y = v[3]
-				airborne = false
-				dy = 0
-			else
-				airborne = true
-			end
+	y = dy + y
+	bx = bdx + bx
+	by = bdy + by
+	if love.keyboard.isDown("down") then
+		dy = dy + 1
+	elseif love.keyboard.isDown("up") then
+		dy = dy - 1
+	else
+		if dy > 0 then
+			dy = dy - 1
+		elseif dy < 0 then
+			dy = dy + 1
 		end
 	end
-	if love.keyboard.isDown("up") then
-		dy = -10
+	if love.keyboard.isDown("right") then
+		dx = dx + 1
+	elseif love.keyboardIsDown("up") then
+		dx = dx - 1
 	end
 
-	
-	if love.keyboard.isDown("right") then
-		dx = 1
-	elseif love.keyboard.isDown("left") then
-		dx = -1
-	else
-		dx = 0
-	end
-	
 end
 
 function love.draw(  )
 	love.graphics.rectangle("fill", x, y, 64, 64)
-	love.graphics.rectangle("fill", 0, 64+120, 128, 10)
-	if airborne then
-		love.graphics.print("hi", 120, 120)
-	end
+	love.graphics.rectangle("fill", bx, by, 10, 10)
 end
