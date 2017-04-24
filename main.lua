9b18b094b7562bfd373534a3ff1201f301382b8b
Julian Kuo-Gross <julian.kuo-gross19@cs81-us-ct3-19.local>
2017-04-24 10:05:31 -0400

    platorms almost work

--- main.lua
+++ main.lua
@@ -1,25 +1,35 @@
 function love.load(  )
-	x = 0
+	x = 1
 	y = 0
+	dx = 0
+	dy = 1
 	tableOfFloors = {{64, 64, 24}}
 	airborne = true
 end
 
 function love.update( dt )
 	if (airborne) then
-		y = y + 1
-		for i,v in ipairs(tableOfFloors) do
-			if	y > v[3] then
-				y = v[3]
-				airborne = false
-			end 
+		y = y + dy
+	end
+	for i,v in ipairs(tableOfFloors) do
+		if	y > v[3] and x > v[1] - 64 and x < v[2] + 64 then
+			y = v[3]
+			airborne = false
+		else
+			airborne = true
 		end
 	end
 	if love.keyboard.isDown("right") then
 		x = x + 1
 	end
+	if love.keyboard.isDown("left") then
+		x = x - 1
+	end
+	if love.keyboard.isDown("up") then
+	end
 end
 
 function love.draw(  )
 	love.graphics.rectangle("fill", x, y, 64, 64)
+	love.graphics.print(x, 100, 100)
 end
